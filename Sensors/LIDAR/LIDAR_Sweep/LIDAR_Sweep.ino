3769e94a3b218e62bfd47d1ed5310b8111eef6f2
Jacob Anderson <jacobanderson1985@Jakes-Mactop.local>
2016-11-03 18:20:03 -0600

    Update to LIDAR_Sweep. Includes bin analysis with 3 bins

--- Sensors/LIDAR/LIDAR_Sweep/LIDAR_Sweep.ino
+++ Sensors/LIDAR/LIDAR_Sweep/LIDAR_Sweep.ino
@@ -11,219 +11,180 @@
 // Name Servos:
 Servo Lidar;
 
-// Global Variables;
-int LIDARmin;           // Minimum reading from LIDAR
+// Define Variables;
+int DISTANCE;           // Raw distance recived from LIDAR
+int LIDARbinMax;        // Max value of LIDAR averaging bins
+int MaxBin = 0;         // Bin number of Max LIDAR averaging bin
 int MaxBinAngle;        // Angle coresponding to Max average bin
+int LIDARbinMin;        // Min value of LIDAR averaging bins
+int MinBin = 0;         // Bin number of Min LIDAR averaging bin
 int MinBinAngle;        // Angle coresponding to Min average bin
-int LIDARangle = 0;     // Angle of the minimum value from center the value to the minimum value of the vector
-
-// Vector variables 
-const int LIDARvec  = 21;      // Number of elements in LIDARdistances vector
-const int binNumber = 19;      // Number of elements in LIDARbin vector
+int place;              // Location of the minimum value elimant in the vectoer. Eliment 1 of the vector is 0.
+int LIDARangle = 0;     // Angle of the minimum value from center
+int LIDARmin;           // Minimum value of the vector. Set very high initialy so that logic statments will reduce
+                           //the value to the minimum value of the vector
+//  Vector variables 
+const int LIDARvec = 11;  // Number of elements in LIDARdistances vector
+const int binNumber = 9;  // Number of elements in LIDARbin vector
 
 // Vectors
-int LIDARdistances[LIDARvec];     // Vector to store LIDAR measurments
-int LIDARAngles [ LIDARvec ];  // Vector of Sweep angles coresponding to the distance readings
-int LIDARbins[binNumber];         // Vector for LIDAR averaging bins
+int LIDARdistances[LIDARvec];  // Vector to store LIDAR measurments
+int LIDARbins[binNumber];      // Vector for LIDAR averaging bins
 
-
-
-
-/*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-==== Set up routien ================================================================================================================================================================
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-void setup(){ Serial.begin(9600); //Opens serial connection at 9600bps.  
-              Serial.println("");
-              Serial.println("-------------------------------------------------");
-  
-  // Set up I2C ---------------------------------------------------------------------------------------------------------------
+// ==== Set up routien =====================================================================================================================
+// =========================================================================================================================================
+void setup(){Serial.begin(9600); //Opens serial connection at 9600bps.   
+  Serial.print(""); // empty line
+  // === Set up LIDAR =================================================================
     
-  I2c.begin();      // Opens & joins the irc bus as master
-  delay(100);       // Waits to make sure everything is powered up before sending or receiving data  
-  I2c.timeOut(50);  // Sets a timeout to ensure no locking up of sketch if I2C communication fails
+  I2c.begin(); // Opens & joins the irc bus as master
+  delay(100); // Waits to make sure everything is powered up before sending or receiving data  
+  I2c.timeOut(50); // Sets a timeout to ensure no locking up of sketch if I2C communication fails
 
-  // Set up servos ------------------------------------------------------------------------------------------------------------
   Lidar.attach(6); // Attache Lidar Servo to pin 6
+  
   Lidar.write(85);  // Centers Lidar detector
   delay (10);
   
-  // Set up LIDAR vecor -------------------------------------------------------------------------------------------------------
+  // === Set up LIDAR vecor ==========================================================
   for (int zero = 0; zero < LIDARvec; zero++) {LIDARdistances[zero] = 0;}
+  
   PrintLIDARvector(); 
-  
-  
-  // Set up LIDAR Angles vector -----------------------------------------------------------------------------------------------
-  int a = 0;
-  for (int angle = -50; angle <= 50; angle += 5) { LIDARAngles[a] = angle; a++;}    // + angle is to the left, - angle is to the right
-  
-  Serial.print(" Angle Vector: [ ");
-  for (int i=0;i<(LIDARvec-1);i++) { Serial.print(LIDARAngles[i]), Serial.print(", ");}
-  Serial.print(LIDARAngles[(LIDARvec-1)]), Serial.println(" ]");
-                                    
-
-  Serial.println("");
-  Serial.println("----------- Set up done -------------");
-  Serial.println("");
+  Serial.println("Set up done");
   
 }
-
-
-
-
-
-/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-======= Main Loop =================================================================================================================================================================
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-
+// ==== Main Loop ===========================================================================================================================
+// ===========================================================================================================================================
 void loop(){ 
     
   LIDARsweep();
+    
+  // Print Distance
   PrintLIDARvector();
-  
-  int input_1 = LIDARanalysis();        Serial.print("input 1: "); 
-  int input_2 = LIDARbinAnalysis();
-  
+  LIDARanalysis();
+  LIDARbinAnalysis();
  delay (1000);
 }
 
 
 
+// ===== Subrutiens ==========================================================================================================================
+// ===========================================================================================================================================
 
-
-/*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-======== Subrutiens ================================================================================================================================================================
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-//---- Sweep LIDAR -------------------------------------------------------------------------------------------------------------------------------------------------
 void LIDARsweep(){ // LIDAR servo is far right at 0 and far left at 180
   
-                   Serial.println("LIDAR is scanning...");
-                   int a =0;
-                   int angle = -50;
-                   for ( int SweepAngle=35; SweepAngle < 140 ; SweepAngle += 5 ) { Lidar. write(SweepAngle); 
-                                                                                   delay(100);
+  Serial.println("LIDAR is scanning...");
+  int a =0;
+  for ( int SweepAngle=35; SweepAngle < 145 ; SweepAngle+=10 )
+  { Lidar. write(SweepAngle);
+    delay(100);
     
-                                                                                   LIDARdistances[a] = random(1,300); // GetDistance();
+    LIDAR();
+    LIDARdistances[a] = DISTANCE;
     
-                                                                                   delay (100);
-                                                                   
-                                                                                   Serial.print ("Sweep Angle: "),      Serial.print(SweepAngle);
-                                                                                   Serial.print ("   \tLIDAR Angle: "), Serial.print(angle);
-                                                                                   Serial.print ("   \tDistance: " ),   Serial.println(LIDARdistances[a]);
-                                                                   
-                                                                                   a++;
-                                                                                   angle += 5; 
-                                                                                  }
-                   Lidar.write(85);
-                   Serial.println("");
+    delay (400);
+    a=a+1; 
+  }
+  Lidar.write(85);
    
-                  }
+}
 
+// ==== Get distance from LIDAR =======================================================
+void LIDAR() {
+  // Write 0x04 to register 0x00
+  uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.write(LIDARLite_ADDRESS,RegisterMeasure, MeasureValue); // Write 0x04 to 0x00
+    delay(1); // Wait 1 ms to prevent overpolling
+  }
 
-
-//---- Get distance from LIDAR ---------------------------------------------------------------------------------------------------------------------------------------
-int GetDistance() {
+  byte distanceArray[2]; // array to store distance bytes from read function
   
-                    // Write 0x04 to register 0x00
-                    uint8_t nackack = 100; // Setup variable to hold ACK/NACK resopnses     
-                    while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
-                                          nackack = I2c.write(LIDARLite_ADDRESS,RegisterMeasure, MeasureValue); // Write 0x04 to 0x00
-                                          delay(1); // Wait 1 ms to prevent overpolling
-                                         }
-
-                    byte distanceArray[2]; // array to store distance bytes from read function
+  // Read 2byte distance from register 0x8f
+  nackack = 100; // Setup variable to hold ACK/NACK resopnses     
+  while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
+    nackack = I2c.read(LIDARLite_ADDRESS,RegisterHighLowB, 2, distanceArray); // Read 2 Bytes from LIDAR-Lite Address and store in array
+    delay(1); // Wait 1 ms to prevent overpolling
+  }
+  int distance = (distanceArray[0] << 8) + distanceArray[1];  // Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
+  DISTANCE = distance;
+}
   
-                    // Read 2byte distance from register 0x8f
-                    nackack = 100; // Setup variable to hold ACK/NACK resopnses     
-                    while (nackack != 0){ // While NACK keep going (i.e. continue polling until sucess message (ACK) is received )
-                                          nackack = I2c.read(LIDARLite_ADDRESS,RegisterHighLowB, 2, distanceArray); // Read 2 Bytes from LIDAR-Lite Address and store in array
-                                          delay(1); // Wait 1 ms to prevent overpolling
-                                         }
+// === Print Lidar Vector ==================================================================
+
+void PrintLIDARvector(){
+ 
+  Serial.print(" LIDAR Vector: [ ");
+  for (int i=0;i<(LIDARvec-1);i++)
+  { Serial.print(LIDARdistances[i]);
+    Serial.print(", ");}
+    Serial.print(LIDARdistances[(LIDARvec-1)]);
+    Serial.println(" ]");
+}
+
+// ==== Analyse Lidar data for minimum value and its angle ==============================================
+void LIDARanalysis(){
+  LIDARmin = 1000;
+  // Find the minimum Value in vector
+  for(int b =0; b < LIDARvec; b++){  
+    if( LIDARdistances[b] < LIDARmin){ LIDARmin = LIDARdistances[b]; place = b;}
+   } 
   
-                    int distance = (distanceArray[0] << 8) + distanceArray[1];  // Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
+  // Calculate angle in degrees from center. + angle is to the left, - angle is to the right
+  if      ( place <= 4) { LIDARangle = (place-5)*10;}
+  else if ( place ==  5) { LIDARangle = 0;}
+  else                  { LIDARangle = -(5-place)*10;}
  
-                    if (distance == 0) { distance = 3000;}
-                    else               { distance = distance;}
+ // Display the calculated values
+   Serial.print("Vector Min: ");
+   Serial.print(LIDARmin);
+   Serial.print("\t Place: ");
+   Serial.print(place);
+   Serial.print("\t Angle: ");
+   Serial.println(LIDARangle);
+   Serial.println(""); // Empty line
+
+}
+// ---- LIDAR bin  analysis -------------------------------------------------------------------------------
+// Takes the readings in the LIDAR vector and averages 3 readings togeter into a bin.
+// The bins over lap by two element and corespond to angles conterclock wise around the
+// front of the robot starting far right.
+
+void LIDARbinAnalysis(){
  
-                    return distance;
-                  }
+ // Calculate LIDAR bins
+ for(int bin = 0; bin < 9; bin++){
+ LIDARbins[bin]= (LIDARdistances[bin]+LIDARdistances[(bin+1)]+LIDARdistances[(bin+2)])/3;  
+  Serial.print ("Bin ");
+  Serial.print (bin);
+  Serial.print (": ");
+  Serial.println (LIDARbins[bin]); 
+ }
  
+
+ LIDARbinMax = 0;        // Set LIDAR Max and min values to quatantied that will be exceeded by actual reading
+ LIDARbinMin = 10000;
  
-  
-//---- Print Lidar Vector --------------------------------------------------------------------------------------------------------------------------------------------
-void PrintLIDARvector(){ Serial.print("LIDAR Vector: [ ");
-                         for (int i=0;i<(LIDARvec-1);i++) { Serial.print(LIDARdistances[i]), Serial.print(", ");}
-                         Serial.print(LIDARdistances[(LIDARvec-1)]), Serial.println(" ]");
-                        }
+ // Find minimum bin value and corespondind bin number
+ for(int bin = 0; bin<9; bin++) {
+  if(LIDARbinMin > LIDARbins[bin]) {LIDARbinMin = LIDARbins[bin]; MinBin = bin;}
+  if(LIDARbinMax < LIDARbins[bin]) {LIDARbinMax = LIDARbins[bin]; MaxBin = bin;}
+ }
+ 
+ // Assign anblges to the bin places    + Angles are left of center, - Angles are right of center
+ if      (MinBin <=3)  {MinBinAngle = -(50-MinBin*10);}
+ else if (MinBin == 4) {MinBinAngle = 0;}
+ else                  {MinBinAngle = MinBin*10-40;}
+ 
+ if      (MaxBin <=3)  {MaxBinAngle = -(50-MaxBin*10);}
+ else if (MaxBin == 4) {MaxBinAngle = 0;}
+ else                  {MaxBinAngle = MaxBin*10-40;}
+
+  // Dispaly values
+  Serial.print("Minimum bin value: "); Serial.print(LIDARbinMin); Serial.print("\t Bin#: "); Serial.print (MinBin); Serial.print("\t Angle: "); Serial.println(MinBinAngle);
+  Serial.print("Maximum bin value: "); Serial.print(LIDARbinMax); Serial.print("\t Bin#: "); Serial.print (MaxBin); Serial.print("\t Angle: "); Serial.println(MaxBinAngle);
+  Serial.println(""); // Blank Line
+}
 
 
 
-
-//---- Analyse Lidar data for minimum value and its angle -----------------------------------------------------------------------------------------------------------
-int LIDARanalysis(){
-                      int place;
-                      LIDARmin = 10000;
-  
-                      // Find the minimum Value in vector
-                      for(int a =0; a < LIDARvec; a++){ if( LIDARdistances[a] < LIDARmin) { LIDARmin = LIDARdistances[a]; place = a;} } 
-  
-                      LIDARangle = LIDARAngles[place];    // Angle that coresponds to the minimum value
-  
-                      // Display the calculated values
-                       Serial.print("Vector Min: "), Serial.print(LIDARmin);
-                       Serial.print("\t Angle: "),   Serial.println(LIDARangle);
-                       Serial.println(""); // Empty line
-                       
-                       if      (LIDARmin > 200)                    {return 0;}
-                       else if (LIDARangle > 40 && LIDARmin <50)   {return 0;}
-                       else if (LIDARangle < -40 && LIDARmin > 50) {return 0;}
-                       else                                        {return 1;}
-                      }
-                      
-                      
-                      
-//---- LIDAR bin  analysis ----------------------------------------------------------------------------------------------------------------------------------------
-/* Takes the readings in the LIDAR vector and averages 3 readings togeter into a bin.
-   The bins over lap by two element and corespond to angles conterclock wise around the
-   front of the robot starting far right.
-*/
-int LIDARbinAnalysis(){
-                         int BinMax = 0;        // Set LIDAR Max and min values to quatantied that will be exceeded by actual reading
-                         int BinMin = 10000;
-                         int Max = 0;             // Bin number of Max LIDAR averaging bin
-                         int Min = 0;             // Bin number of Min LIDAR averaging bin
-
-
- 
-                         // Calculate LIDAR bins
-                         int angle = -45;
-                         for(int bin = 0; bin < binNumber; bin++){ LIDARbins[bin]= (LIDARdistances[bin]+LIDARdistances[(bin+1)]+LIDARdistances[(bin+2)])/3;  
-                                                                   
-                                                                   Serial.print ("Bin: "),     Serial.print (bin);
-                                                                   Serial.print ("\tAngle: "), Serial.print(angle);
-                                                                   Serial.print ("\tValue: "), Serial.println (LIDARbins[bin]);
-                                                                  
-                                                                   angle += 5; 
-                                                                  }
-                         Serial.println("");
-
- 
-                         // Find minimum bin value and corespondind bin number
-                         for(int bin = 0; bin < binNumber; bin++) { if(BinMin > LIDARbins[bin]) {BinMin = LIDARbins[bin]; Min = bin;}
-                                                                    if(BinMax < LIDARbins[bin]) {BinMax = LIDARbins[bin]; Max = bin;}
-                                                                   }
- 
-                         // Assign anblges to the bin places    + Angles are left of center, - Angles are right of center
-                         MinBinAngle = LIDARAngles[Min+1];
-                         MaxBinAngle = LIDARAngles[Max+1];
- 
-                         // Dispaly values
-                         Serial.print("Minimum bin value: "); Serial.print(BinMin); Serial.print("\t Bin#: "); Serial.print (Min); Serial.print("\t Angle: "); Serial.println(MinBinAngle);
-                         Serial.print("Maximum bin value: "); Serial.print(BinMax); Serial.print("\t Bin#: "); Serial.print (Max); Serial.print("\t Angle: "); Serial.println(MaxBinAngle);
-                         Serial.println(""); // Blank Line
-                         
-                         if (BinMin > 100) {return 0;}
-                         else              {return 1;}
-                        }
